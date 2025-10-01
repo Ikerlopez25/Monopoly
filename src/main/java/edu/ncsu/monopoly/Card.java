9c52f7d94bfd4f2dc0330ca8f864724834099d7a
Reki252 <99650561+Reki252@users.noreply.github.com>
2025-10-01 16:35:10 +0200

    nuevo comentario x3

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,5 +1,7 @@
 package edu.ncsu.monopoly;
-//añadimos cambio 
+//añadimos cambio
+//añadimos comentario nuevoo
+//comentario nusdfsdfsdfsdfevooo
 public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
